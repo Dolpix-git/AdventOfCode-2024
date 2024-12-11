ba0597afd7e1da9adecf937c8c561d4dcad1ffd9
Dolpix <kikigoldwolf@gmail.com>
2024-12-11 19:24:38 +0000

    Feat: Added day three part one

--- Days/Day3/DayThreePartOne.cpp
+++ Days/Day3/DayThreePartOne.cpp
@@ -2,4 +2,42 @@
 // Created by Dolpix on 12/11/2024.
 //
 
+#include <iostream>
+#include <regex>
+#include <limits>
 #include "DayThreePartOne.hpp"
+
+void DayThreePartOne::Execute() {
+    std::string inputLine;
+
+    std::cout << "Enter corrupted memory string:\n";
+    std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+    std::string line;
+    while (std::getline(std::cin, line)) {
+        if (line == "STOP") {
+            break;
+        }
+        inputLine += line + "\n";
+    }
+    std::cout << "\n";
+
+    std::regex mulRegex(R"(mul\((\d+),(\d+)\))");
+    std::smatch match;
+
+    int totalProduct = 0;
+
+    auto searchStart = inputLine.cbegin();
+    while (std::regex_search(searchStart, inputLine.cend(), match, mulRegex)) {
+        int num1 = std::stoi(match[1].str());
+        int num2 = std::stoi(match[2].str());
+
+        int product = num1 * num2;
+        totalProduct += product;
+        std::cout << totalProduct << std::endl;
+        std::cout << "Found: " << match[0] << " => " << num1 << " * " << num2 << " = " << product << "\n";
+
+        searchStart = match.suffix().first;
+    }
+
+    std::cout << "Total Product: " << totalProduct << "\n";
+}
--- Days/Day3/DayThreePartOne.hpp
+++ Days/Day3/DayThreePartOne.hpp
@@ -5,10 +5,17 @@
 #ifndef UNTITLED_DAYTHREEPARTONE_HPP
 #define UNTITLED_DAYTHREEPARTONE_HPP
 
+#include "../Day.h"
+#include <vector>
 
-class DayThreePartOne {
-
+class DayThreePartOne : public Day {
+public:
+    void Execute() override;
 };
 
+static const bool dayThreePartOneRegistered = [] {
+    Day::Register("Day3 Part1", []() { return std::make_unique<DayThreePartOne>(); });
+    return true;
+}();
 
 #endif //UNTITLED_DAYTHREEPARTONE_HPP
